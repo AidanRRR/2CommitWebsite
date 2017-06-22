091f2a8d20c67f2bfbf39a2ad7a5803ab1464c2b
DESKTOP-8OUDKEN\Aidan <aidan.rypens@outlook.com>
2017-06-22 21:27:18 +0200

    Deployment 

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-var browserSync = require('browser-sync').create();
+/**
+ * Module dependencies.
+ */
 
-browserSync.init({
-    server: {
-        baseDir: 'app'
-    },
-});
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var app = express();
+var viewDir = 'app';
+
+// log requests
+//app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, viewDir)));
+
+app.listen(3000);
+console.log('listening on port 3000');
