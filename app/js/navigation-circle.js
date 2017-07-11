670fcdf3f9b5626d58e733e11fbd6e47d2fd8886
DESKTOP-8OUDKEN\Aidan <aidan.rypens@outlook.com>
2017-07-11 10:06:35 +0200

    menu responsive

--- app/js/navigation-circle.js
+++ app/js/navigation-circle.js
@@ -1,25 +1,45 @@
-(function () {
+function setPosition() {
+    // sugar beallitasa/set radius
+    var r = 200;
 
-    var button = document.getElementById('smartcms_button'),
-        wrapper = document.getElementById('smartcms_wrapper');
+    // linkek beallitasa / set anchor elements
+    var links = $('.center-circle').find('.little-circle');
 
-    //open and close menu when the button is clicked
-    var open = false;
-    button.addEventListener('click', handler, false);
+    // linkek szamlalasa / count anchor elements
+    var counter = links.length - 1;
 
-    function handler() {
-        if (!open) {
-            this.innerHTML = "Close";
-            classie.add(wrapper, 'opened-nav');
-        }
-        else {
-            this.innerHTML = "Menu";
-            classie.remove(wrapper, 'opened-nav');
-        }
-        open = !open;
-    }
-    function closeWrapper() {
-        classie.remove(wrapper, 'opened-nav');
-    }
+    // lepesszog beallitasa / set angle steps
+    var step = 180 / counter;
 
-})();
+    // kezdeti szog beallitasa / set starting angle
+    var angle = 0 - (180 / counter);
+
+    // radianba konvertalas / convert to radian
+    var rad = angle * (Math.PI / 180);
+
+    var x = 0;
+    var y = 0;
+
+    // ciklus futtatasa a linkeken / loop in links 
+    $.each(links, function () {
+        angle += step;
+        rad = angle * (Math.PI / 180);
+        x = Math.round(r * Math.cos(rad)) + 125;
+        y = Math.round(r * Math.sin(rad));
+        //$(this).css({ left: x + 'px', 'margin-top': -1*y + 'px' });
+        $(this).animate({ left: x + 'px', 'margin-top': -1 * y + 'px' });
+    });
+
+    links.hover(function (e) {
+        e.preventDefault();
+        var name = $(this).data('name');
+        console.log(name);
+        var link = $(this).attr('href');
+        $('.categories a').text(name);
+        $('.categories a').attr('href', link);
+    });
+}
+
+$(document).ready(function () {
+    setPosition();
+});
