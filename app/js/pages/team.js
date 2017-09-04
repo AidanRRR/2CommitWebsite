4e21112729069c19a58257dc4c95c61e2b742400
Rypens Aidan <rypenai@cronos.be>
2017-09-04 17:01:20 +0200

    Team pagina

--- app/js/pages/team.js
+++ app/js/pages/team.js
@@ -1,5 +1,6 @@
 var rotation = 0;
 const rotationStep = 40;
+const loopSpeed = 5000;
 
 function rotateLeft() {
     var from = this.rotation;
@@ -22,12 +23,12 @@ function AnimateRotate(from, to) {
     }).animate({
         deg: to
     }, {
-            step: function (now, fx) {
-                $("#planet").css({
-                    transform: "rotate(" + now + "deg)"
-                });
-            }
-        });
+        step: function (now, fx) {
+            $("#planet").css({
+                transform: "rotate(" + now + "deg)"
+            });
+        }
+    });
 
     AnimateSpaceman();
 }
@@ -47,30 +48,70 @@ function AnimateSpaceman() {
     }
 }
 
+function Loop() {
+    setInterval(function () {
+        rotateRight();
+    }, loopSpeed);
+}
+
 var vm = new Vue({
     el: '#team-info',
     data: {
         team_index: 0,
         members: [{
-            "name": "Katia",
-            "title": "Account Manager",
-            "quote": "My only focus is satisfaction and trust of our customers and our team."
-        },
-        {
-            "name": "Koen",
-            "title": "Project Manager",
-            "quote": "There is no I in team!"
-        },
-        {
-            "name": "Dimitri",
-            "title": ".NET Architect",
-            "quote": "Every day, work to refine skills you have and to add new tools to your repertoire."
-        },
-        {
-            "name": "Stijn",
-            "title": ".NET Architect",
-            "quote": "Flying is learning how to throw yourself at the ground and miss"
-        }
+                "name": "Katia",
+                "title": "Account Manager",
+                "quote": "My only focus is satisfaction and trust of our customers and our team.",
+                "url": "images/avatars/katia.png"
+            },
+            {
+                "name": "Koen",
+                "title": "Project Manager",
+                "quote": "There is no I in team!",
+                "url": "images/avatars/koen.png"
+            },
+            {
+                "name": "Dimitri",
+                "title": ".NET Architect",
+                "quote": "Every day, work to refine skills you have and to add new tools to your repertoire.",
+                "url": "images/avatars/dimitri.png"
+            },
+            {
+                "name": "Stijn",
+                "title": ".NET Architect",
+                "quote": "Flying is learning how to throw yourself at the ground and miss",
+                "url": "images/avatars/stijn.png"
+            },
+            {
+                "name": "Aidan",
+                "title": "",
+                "quote": "",
+                "url": "images/avatars/aidan.png"
+            },
+            {
+                "name": "Richard",
+                "title": "",
+                "quote": "",
+                "url": "images/avatars/richard.png"
+            },
+            {
+                "name": "Kristof",
+                "title": "",
+                "quote": "",
+                "url": "images/avatars/kristof.png"
+            },
+            {
+                "name": "Bianca",
+                "title": "",
+                "quote": "",
+                "url": "images/avatars/bianca.png"
+            },
+            {
+                "name": "Tanguy",
+                "title": "",
+                "quote": "",
+                "url": "images/avatars/tanguy.png"
+            }
         ]
     },
     methods: {
