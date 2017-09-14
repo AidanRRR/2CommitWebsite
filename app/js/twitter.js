25e7e0862fc34d194a86aee52cd163053426d20e
Rypens Aidan <rypenai@cronos.be>
2017-09-14 12:35:40 +0200

    Twitter TV

--- app/js/twitter.js
+++ app/js/twitter.js
@@ -2,14 +2,15 @@ new Vue({
     el: '#twitter',
     data: {
         tweets: '',
-        tweetIndex: 0
+        tweetIndex: 0,
+        tweetsLoopSpeed: 5000
     },
     methods: {
         getTweets: function () {
             this.$http.get('/api/tweets').then(response => {
                 if (response.statusText == "OK") {
                     this.tweets = response.body;
-                    setInterval(this.getNextTweet(), 5000);
+                    this.getNextTweet();
                 }
             });
         },
@@ -18,20 +19,28 @@ new Vue({
 
             let tweetDomTitle = "";
             let tweetDomImg = "";
+            tweetDomTitle = "<span class='tweet-text'>" + this.tweets[this.tweetIndex].text + "</span><br />";
 
-            for (let tweet of this.tweets) {
-                if (this.tweetIndex === index) {
-                    tweetDomTitle = "<span class='tweet-text'>" + tweet.text + "</span><br />";
-                    if (tweet.entities.media != undefined) {
-                        tweetDomImg = "<img src='" + tweet.entities.media[0].media_url + "' class='tweet-img'>"
-                        $("#tweets-text-container").append(tweetDomTitle);
-                        $("#tweets-text-container").append(tweetDomImg);
-                    } else {
-                        $("#tweets-text-container").append(tweetDomTitle);
-                    }
-                }
+            $("#twitter-box").removeClass("tweets-loading").addClass("tweets");
+
+            $("#tweets-text-container").empty();
+            if (this.tweets[this.tweetIndex].entities.media != undefined) {
+                tweetDomImg = "<img src='" + this.tweets[this.tweetIndex].entities.media[0].media_url + "' class='tweet-img'>"
+                $("#tweets-text-container").append(tweetDomTitle);
+                $("#tweets-text-container").append(tweetDomImg);
+            } else {
+                //tweetDomImg = "<img src='" + "http://www.rypens.be/upload/files/Logo-07.png" + "' class='tweet-img'>"
+                $("#tweets-text-container").append(tweetDomTitle);
+                //$("#tweets-text-container").append(tweetDomImg);
             }
 
+            if (this.tweetIndex < 9) {
+                setTimeout(function () {
+                    $("#tweets-text-container").empty;
+                    $("#twitter-box").removeClass("tweets").addClass("tweets-loading");
+                }, this.tweetsLoopSpeed - 500);
+                setTimeout(this.getNextTweet, this.tweetsLoopSpeed);
+            }
         }
     },
     beforeMount() {
