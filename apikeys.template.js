bdf5b412c5291f9ddc36823b8e9eb07cc644f8f9
Aidan <aidan.rypens@outlook.com>
2018-02-21 20:52:53 +0100

    Spaceman azure page

--- apikeys.template.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    sendgrid: '',
-    AZURE_STORAGE_CONNECTION_STRING: '',
-    TWITTER_CONSUMER_KEY: '',
-    TWITTER_CONSUMER_SECRET: '',
-    TWITTER_ACCESS_TOKEN_KEY: '',
-    TWITTER_ACCESS_TOKEN_SECRET: ''
-}
